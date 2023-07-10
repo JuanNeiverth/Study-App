5a94621e2957ddf32cd34edf3c7b629b9cb5b786
Juan marcos Polli neiverth <polli.neiverth@gmail.com>
2023-07-10 09:09:28 -0300

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by Juan on 10/07/23.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
